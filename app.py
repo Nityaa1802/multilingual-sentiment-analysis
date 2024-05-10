951a2bb1806039c560e601bcd85c354cdd0c7352
NityaAgarwal18 <nityaagarwal0218@gmail.com>
2024-05-10 18:55:02 +0530

    changed port

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def my_form_post():
 
 if __name__ == "__main__":
     # app.run(debug=True, host="127.0.0.1", port=5002, threaded=True)
-    app.run(debug=True,host="0.0.0.0",port=os.getenv("PORT"))
+    app.run(debug=True,host="0.0.0.0",port=7005)
